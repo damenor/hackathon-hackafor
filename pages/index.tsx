dbb0686950d245c1e8d9ed3a241dc310a1884c5e
damenor <damenor.develop@gmail.com>
2023-02-22 14:40:07 +0100

    changed settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ const HomePage: NextPage<HomePageProps> = ({ creators }) => {
 export default HomePage
 
 const API_URL = `${process.env.VERCEL_URL}/api`
+console.log({process})
 export const getCreatorsActives = async () => {
   const response = await fetcher({ url: `${API_URL}/creator` })
   return response.data
